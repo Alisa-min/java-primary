eeebd114af0a9b85568de22a18b4df6723f11ba0
Alisa-min <1731604140@qq.com>
2020-03-18 19:40:45 +0800

    test

---
+++ Test2.java
@@ -0,0 +1,27 @@
+public class Test2 {
+
+    public static void main(String[] args) {
+
+
+
+        int a = 10;
+        int b = 5;
+        double c = 1;
+        double d = 2;
+
+        double num = 1.1;
+
+        System.out.println(a/b);
+        System.out.println(c/d);
+        System.out.println(c/a);
+        System.out.println(a/d);
+        System.out.println(num*num);
+
+    }
+
+
+
+
+
+
+}
