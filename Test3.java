454cce79c3c72b0f16d54311e06554bd35238da3
Alisa-min <1731604140@qq.com>
2020-03-18 22:46:57 +0800

    test

---
+++ Test3.java
@@ -0,0 +1,13 @@
+public class Test3 {
+
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+        System.out.println("a = " + a + ",b = " + b );
+    }
+
+
+
+}
