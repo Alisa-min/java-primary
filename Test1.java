3fddb966ee7fcc3fcbb11a97541e07c281ff1a2b
Alisa-min <1731604140@qq.com>
2020-03-18 17:25:23 +0800

    test

---
+++ Test1.java
@@ -0,0 +1,45 @@
+public class Test1{
+
+    public static void main(String[] args) {
+
+
+        //整型变量
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+
+        //长整型变量
+        long num = 10L;//也可写为10l
+        System.out.println(num);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+
+        //单精度浮点型变量
+
+        float a = 1.0f;//也可写为1.0F
+        System.out.println(a);
+
+        //字符型变量
+
+        char b = '哈';
+        System.out.println(b);
+
+        //字节型变量
+
+        byte c = 0;
+        System.out.println(c);
+
+        //短整型变量
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+
+
+        //布尔型变量
+        boolean d = true;
+        System.out.println(d);
+
+
+    }
+
+
+}
