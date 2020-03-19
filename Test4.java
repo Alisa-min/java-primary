179841e28fae33b5206be39a621f20e097df67c6
Alisa-min <1731604140@qq.com>
2020-03-19 22:07:21 +0800

    test

---
+++ Test4.java
@@ -0,0 +1,10 @@
+public class Test4 {
+
+
+    public static void main(String[] args) {
+        byte a1 = 10;
+        byte b1 = 20;
+        byte c = (byte)(a1 + b1);
+        System.out.println(c);
+    }
+}
