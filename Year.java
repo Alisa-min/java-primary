77f72a70b57a6b2a54de1e6367a1d624414f3e9c
Alisa-min <1731604140@qq.com>
2020-03-22 22:15:01 +0800

    test

---
+++ Year.java
@@ -0,0 +1,23 @@
+package lesson2;
+
+public class Year {
+
+    public static void main(String[] args) {
+
+
+
+
+        for(int year=1000;year<=2000;year++){
+            boolean Isleapyear=false;
+            if(year%4==0 && year % 100 !=0){//普通闰年
+                Isleapyear=true;
+            }
+            if(year%400==0){//实际闰年
+                Isleapyear=true;
+            }
+            if(Isleapyear){//是闰年，打印year当前的值
+                System.out.print(year+" ");
+            }
+        }
+    }
+}
