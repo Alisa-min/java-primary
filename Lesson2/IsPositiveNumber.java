c5b3f86bd185e89ae497a4cc42dac4d1fc88d91a
Alisa-min <1731604140@qq.com>
2020-03-23 21:46:04 +0800

    test

---
+++ Lesson2/IsPositiveNumber.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class IsPositiveNumber {
+
+    public static void main(String[] args) {
+        int num = 0;
+        if(num>0){
+            System.out.println(num+"是正数");
+        }else if(num<0){
+            System.out.println(num+"是负数");
+        }else{
+            System.out.println("num是0");
+        }
+    }
+
+
+}
