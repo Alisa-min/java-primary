c59f8018527566a0a74ced4e6f2ebe2a7c4584be
Alisa-min <1731604140@qq.com>
2020-06-10 19:16:15 +0800

    test

---
+++ TestDemo4.java
@@ -0,0 +1,31 @@
+package Lesson4;
+
+public class TestDemo4 {
+
+    public static void hanoi(int n,char pos1,char pos2,char pos3){
+
+        if(n == 1){
+            move(pos1,pos3);
+        }else{
+            hanoi(n-1,pos1,pos3,pos2);
+            move(pos1,pos3);
+            hanoi(n-1,pos2,pos1,pos3);
+
+        }
+    }
+
+    public static void move(char pos1,char pos2){
+        System.out.print(pos1+"-->"+pos2+" ");
+    }
+
+    public static void main(String[] args) {
+
+        hanoi(1,'A','B','C');
+        System.out.println();
+        hanoi(2,'A','B','C');
+        System.out.println();
+        hanoi(3,'A','B','C');
+        System.out.println();
+
+    }
+}
